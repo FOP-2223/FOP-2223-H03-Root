f07af7dad750af4c73fef631bb39d48223c2de78
Steyer, Nick <nick.steyer@stud.tu-darmstadt.de>
2022-09-26 02:38:25 +0200

    Test offspring Getter

--- src/grader/java/h03/TutorTests_H1_2.java
+++ src/grader/java/h03/TutorTests_H1_2.java
@@ -31,6 +31,7 @@ public class TutorTests_H1_2 {
         World.reset();
         World.setDelay(0);
         World.setVisible(false);
+        World.setSize(500, 500);
     }
 
     final String class_name = "RobotWithOffspring";
@@ -47,8 +48,8 @@ public class TutorTests_H1_2 {
     @Test
     @DisplayName("2 | Methode initOffspring")
     // TODO: parametrisierter Test
+    // TODO: nochmal kontrollieren
     public void t02() {
-        World.setSize(500, 500);
         Field offspringField = robotWithOffspringCT.resolve().resolveAttribute(
             new AttributeMatcher("offspring", 0.8, Modifier.PROTECTED,
                 Robot.class));
@@ -77,6 +78,39 @@ public class TutorTests_H1_2 {
         assertEquals(192, ((Robot) offspring).getNumberOfCoins(), "Das Attribut numberOfCoins des offspring-Objekts " +
             "wird nicht korrekt gesetzt.");
     }
+
+    @Test
+    @DisplayName("3 | Offspring-Getter")
+    public void t03() {
+        ClassTester<?> ct = robotWithOffspringCT.resolve();
+        Field offspringField = ct.resolveAttribute(
+            new AttributeMatcher("offspring", 0.8, Modifier.PROTECTED,
+                Robot.class));
+
+        Object robotInstance = robotWithOffspringCT.getClassInstance();
+
+        var offspring = new Robot(129, 472, Direction.RIGHT, 463);
+        assertDoesNotThrow(() -> offspringField.set(robotInstance, offspring));
+
+        testGetterMethod("getXOfOffspring", int.class, ct, 129);
+        testGetterMethod("getYOfOffspring", int.class, ct, 472);
+        testGetterMethod("getDirectionOfOffspring", Direction.class, ct, Direction.RIGHT);
+        testGetterMethod("getNumberOfCoinsOfOffspring", int.class, ct, 463);
+    }
+
+    private void testGetterMethod(String getterName, Class<?> returnType, ClassTester<?> ct, Object expectedValue) {
+        var methodTester = new MethodTester(
+            ct,
+            getterName,
+            0.8,
+            Modifier.PUBLIC,
+            returnType,
+            new ArrayList<>()).verify();
+        var returnValue = methodTester.invoke();
+
+        assertEquals(expectedValue, returnValue, String.format("Falsche Rückgabe der Methode %s.", getterName));
+    }
+
 //
 //    @Test
 //    public void offspringFieldAndInitOffspringMethodCorrectlyImplemented() {
@@ -111,9 +145,11 @@ public class TutorTests_H1_2 {
 //            final var initializedOffspring = (Robot) offspringField.get(sut);
 //
 //            Assertions.assertAll(
-//                () -> assertEquals(4, initializedOffspring.getX(), "Die X-Position des offspring-Roboters ist nicht " +
+//                () -> assertEquals(4, initializedOffspring.getX(), "Die X-Position des offspring-Roboters ist nicht
+//                " +
 //                    "korrekt."),
-//                () -> assertEquals(6, initializedOffspring.getY(), "Die Y-Position des offspring-Roboters ist nicht " +
+//                () -> assertEquals(6, initializedOffspring.getY(), "Die Y-Position des offspring-Roboters ist nicht
+//                " +
 //                    "korrekt."),
 //                () -> assertEquals(Direction.DOWN, initializedOffspring.getDirection(), "Die Blickrichtung des " +
 //                    "offspring-Roboters ist nicht korrekt."),
@@ -148,11 +184,13 @@ public class TutorTests_H1_2 {
 //
 //        World.setSize(5, 5);
 //        var sut = new RobotWithOffspring(5, 5, Direction.UP, 0);
-//        assertFalse(sut.offspringIsInitialized(), "offspringIsInitialized gibt nicht false zurück, wenn der offspring" +
+//        assertFalse(sut.offspringIsInitialized(), "offspringIsInitialized gibt nicht false zurück, wenn der
+//        offspring" +
 //            " noch nicht initialisiert wurde.");
 //        // TODO: was, wenn es initOffspring nicht gibt? keine Punkte dann auch für diese Aufgabe?
 //        sut.initOffspring(Direction.UP, 0);
-//        assertTrue(sut.offspringIsInitialized(), "offspringIsInitialized gibt nicht true zurück, wenn der offspring " +
+//        assertTrue(sut.offspringIsInitialized(), "offspringIsInitialized gibt nicht true zurück, wenn der offspring
+//        " +
 //            "initialisiert wurde.");
 //    }
 }
