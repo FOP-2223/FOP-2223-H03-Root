eecdeb2e3bc8a3387342869aef1c4201431905e3
Steyer, Nick <nick.steyer@stud.tu-darmstadt.de>
2022-10-12 10:13:23 +0200

    Enhance tests for 1.3

--- src/grader/java/h03/TutorTests_H1_3.java
+++ src/grader/java/h03/TutorTests_H1_3.java
@@ -6,9 +6,9 @@ import fopbot.Robot;
 import fopbot.World;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.sourcegrade.jagr.api.rubric.TestForSubmission;
 import org.tudalgo.algoutils.reflect.AttributeMatcher;
@@ -37,22 +37,21 @@ public class TutorTests_H1_3 {
         World.setVisible(false);
     }
 
+    // DONE
     @ParameterizedTest
-    @CsvSource({"12,32,UP,34,44,56"})
-    @DisplayName("1 | Methode addToXOfOffspring")
-    // TODO: Parametrisierter Test
-    // TODO: test multiple cases for too big, too small etc.
-    public void t01(int x, int y, Direction direction, int numberOfCoins, int xToAssign, int expectedResultX) {
+    @CsvFileSource(resources = "/TutorTests_H1_3-addToXOfOffspringDeclaredAndImplementedCorrectly.csv", numLinesToSkip = 1)
+    @DisplayName("Methode \"addToXOfOffspring\" wurde korrekt deklariert und implementiert.")
+    public void addToXOfOffspringDeclaredAndImplementedCorrectly(int x, int y, Direction direction, int numberOfCoins,
+                                                                 int xToAssign, int expectedResultX) throws IllegalAccessException {
         var offspring = new Robot(x, y, direction, numberOfCoins);
         testAddToMethod("addToXOfOffspring", FieldEntity::getX, offspring, xToAssign, expectedResultX, true);
     }
 
+    // DONE
     @ParameterizedTest
-    @CsvSource({"12,32,UP,34,44,76"})
-    @DisplayName("2 | Methode addToYOfOffspring")
-    // TODO: Parametrisierter Test
-    // TODO: test multiple cases for too big, too small etc.
-    public void t02(int x, int y, Direction direction, int numberOfCoins, int yToAssign, int expectedResultY) {
+    @CsvFileSource(resources = "/TutorTests_H1_3-addToYOfOffspringDeclaredAndImplementedCorrectly.csv", numLinesToSkip = 1)
+    @DisplayName("Methode \"addToYOfOffspring\" wurde korrekt deklariert und implementiert.")
+    public void addToYOfOffspringDeclaredAndImplementedCorrectly(int x, int y, Direction direction, int numberOfCoins, int yToAssign, int expectedResultY) throws IllegalAccessException {
         var offspring = new Robot(x, y, direction, numberOfCoins);
         testAddToMethod("addToYOfOffspring", FieldEntity::getY, offspring, yToAssign, expectedResultY, true);
     }
@@ -63,7 +62,7 @@ public class TutorTests_H1_3 {
     // TODO: Parametrisierter Test
     // TODO: test multiple cases for too big, too small etc.
     public void t03(int x, int y, Direction direction, int numberOfCoins, int directionToAssign,
-                    Direction expectedResultDirection) {
+                    Direction expectedResultDirection) throws IllegalAccessException {
         var offspring = new Robot(x, y, direction, numberOfCoins);
         testAddToMethod("addToDirectionOfOffspring", Robot::getDirection, offspring, directionToAssign,
             expectedResultDirection, false);
@@ -74,7 +73,7 @@ public class TutorTests_H1_3 {
     @DisplayName("4 | Methode addToNumberOfCoinsOfOffspring")
     // TODO: Parametrisierter Test
     // TODO: test multiple cases for too big, too small etc.
-    public void t04(int x, int y, Direction direction, int numberOfCoins, int coinsToAssign, int expectedResultCoins) {
+    public void t04(int x, int y, Direction direction, int numberOfCoins, int coinsToAssign, int expectedResultCoins) throws IllegalAccessException {
         var offspring = new Robot(x, y, direction, numberOfCoins);
         testAddToMethod("addToNumberOfCoinsOfOffspring", Robot::getNumberOfCoins, offspring, coinsToAssign,
             expectedResultCoins, false);
@@ -82,13 +81,14 @@ public class TutorTests_H1_3 {
     }
 
     private <T> void testAddToMethod(String methodName, @NotNull ValueGetter<T> valueGetter, Robot offspring,
-                                     int valueToAssign, T expectedResultValue, boolean setWorldSize) {
+                                     int valueToAssign, T expectedResultValue, boolean setWorldSize) throws IllegalAccessException {
         ClassTester<?> ct = robotWithOffspringCT.resolve();
         Field offspringField = ct.resolveAttribute(
             new AttributeMatcher("offspring", 0.8, Robot.class));
 
         assertFalse(offspringField.getType().isArray(),
-            String.format("Der Datentyp von Attribut \"%s\" ist ein Array, sollte aber kein Array sein.", offspringField.getName()));
+            String.format("Der Datentyp von Attribut \"%s\" ist ein Array, sollte aber kein Array sein.",
+                offspringField.getName()));
 
         Object robotInstance = robotWithOffspringCT.getClassInstance();
 
@@ -99,7 +99,7 @@ public class TutorTests_H1_3 {
                 String.format("Der Datentyp von Attribut \"%s\" ist ein Array, sollte aber kein Array sein.",
                     numberOfColumnsOfWorldField.getName()));
 
-            assertDoesNotThrow(() -> numberOfColumnsOfWorldField.setAccessible(true));
+            numberOfColumnsOfWorldField.setAccessible(true);
             assertDoesNotThrow(() -> numberOfColumnsOfWorldField.set(robotInstance, World.getWidth()));
 
             Field numberOfRowsOfWorldField = ct.resolveAttribute(
@@ -108,29 +108,27 @@ public class TutorTests_H1_3 {
                 String.format("Der Datentyp von Attribut \"%s\" ist ein Array, sollte aber kein Array sein.",
                     numberOfRowsOfWorldField.getName()));
 
-            assertDoesNotThrow(() -> numberOfRowsOfWorldField.setAccessible(true));
+            numberOfRowsOfWorldField.setAccessible(true);
             assertDoesNotThrow(() -> numberOfRowsOfWorldField.set(robotInstance, World.getHeight()));
         }
 
-        var methodTester = new MethodTester(
-            ct,
-            methodName,
-            0.8,
-            Modifier.PUBLIC,
-            void.class,
+        var methodTester = new MethodTester(ct, methodName, 0.8, Modifier.PUBLIC, void.class,
             new ArrayList<>(List.of(
                 new ParameterMatcher("foobar", 0, int.class)
             ))).verify();
 
         assertDoesNotThrow(() -> methodTester.invoke(13),
-            String.format("Die Methode %s wirft eine Exception wenn offspring nicht initialisiert ist.",
+            String.format("Die Methode \"%s\" wirft eine Exception, wenn der offspring nicht initialisiert ist.",
                 methodName));
 
-        assertDoesNotThrow(() -> offspringField.set(robotInstance, offspring));
+        offspringField.setAccessible(true);
+        offspringField.set(robotInstance, offspring);
+
         assertDoesNotThrow(() -> methodTester.invoke(valueToAssign));
+
         var newValue = valueGetter.getValue(offspring);
         assertEquals(expectedResultValue, newValue,
-            String.format("Die Methode %s weist dem offspring den falschen Attributwert zu.", methodName));
+            String.format("Die Methode \"%s\" weist dem offspring den falschen Attributwert zu.", methodName));
     }
 
     private interface ValueGetter<T> {
