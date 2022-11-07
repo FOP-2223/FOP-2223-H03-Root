fc50b54f3efb94a36cf9eef09bf475f8be920f4a
Daniel Mangold <mangold.daniel@protonmail.com>
2022-11-07 01:29:42 +0100

    H3.1 tests WIP

--- src/graderPrivate/java/h03/TutorTests_H3_1.java
+++ src/graderPrivate/java/h03/TutorTests_H3_1.java
@@ -2,87 +2,109 @@ package h03;
 
 import fopbot.Direction;
 import fopbot.World;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import h03.transform.ClassTransformerTemplate;
+import h03.transform.H3_1_Transformers;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.sourcegrade.jagr.api.rubric.TestForSubmission;
+import org.sourcegrade.jagr.api.testing.TestCycle;
+import org.sourcegrade.jagr.api.testing.extension.TestCycleResolver;
 import org.tudalgo.algoutils.reflect.AttributeMatcher;
 import org.tudalgo.algoutils.reflect.ClassTester;
-import org.tudalgo.algoutils.reflect.MethodTester;
 import org.tudalgo.algoutils.reflect.ParameterMatcher;
+import org.tudalgo.algoutils.tutor.general.assertions.Context;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
-import java.util.List;
 
 import static h03.H03_Class_Testers.robotWithOffspring2CT;
 import static h03.H03_Class_Testers.robotWithOffspringCT;
 import static h03.H03_Class_Testers.twinRobotsCT;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.*;
 
 @TestForSubmission
-@DisplayName("H3.1")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TutorTests_H3_1 {
+
+    private static Field robotsField;
+
     @BeforeAll
     public static void setup() {
         World.reset();
         World.setSize(500, 500);
         World.setDelay(0);
         World.setVisible(false);
+
+        robotWithOffspringCT.resolve();
+        robotWithOffspring2CT.resolve();
+        twinRobotsCT.resolve();
     }
 
     // DONE
     @Test
+    @Order(1)
     @DisplayName("Attribut \"robots\" wurde korrekt deklariert.")
     public void robotsDeclaredCorrectly() {
-        var attribute = twinRobotsCT.resolve().resolveAttribute(
-            new AttributeMatcher("robots", 1, Modifier.PRIVATE | Modifier.FINAL,
-                robotWithOffspringCT.assureClassResolved().getClass()));
+        robotsField = twinRobotsCT.resolve().resolveAttribute(
+            new AttributeMatcher("robots", 1, Modifier.PRIVATE, robotWithOffspringCT.assureClassResolved().getClass()));
 
-        assertTrue(attribute.getType().isArray(),
-            "Der Datentyp von Attribut \"robots\" ist kein Array, sollte aber ein Array sein.");
+        assertTrue(robotsField.getType().isArray(), emptyContext(),
+            result -> "expected field robots to be an array but it isn't");
+        robotsField.trySetAccessible();
     }
 
     @ParameterizedTest
+    @Order(2)
     @CsvSource({"12,32"})
     @DisplayName("2 | Konstruktor")
+    @ExtendWith(TestCycleResolver.class)
     @SuppressWarnings("unchecked")
-    public void t02(int numberOfColumnsOfWorld, int numberOfRowsOfWorld) {
-        var numberOfColumnsOfWorldParameterMatcher = new ParameterMatcher("numberOfColumnsOfWorld", 0.8, int.class);
-        var numberOfRowsOfWorldParameterMatcher = new ParameterMatcher("numberOfRowsOfWorld", 0.8, int.class);
+    public void t02(int numberOfColumnsOfWorld, int numberOfRowsOfWorld, TestCycle testCycle) throws ReflectiveOperationException {
+        if (robotsField == null) {
+            fail(emptyContext(), result -> "Field robots could not be resolved");
+        }
 
-        var constructor = (Constructor<Object>) twinRobotsCT.assureClassResolved().resolveConstructor(
+        ParameterMatcher numberOfColumnsOfWorldParameterMatcher = new ParameterMatcher("numberOfColumnsOfWorld", 0.8, int.class);
+        ParameterMatcher numberOfRowsOfWorldParameterMatcher = new ParameterMatcher("numberOfRowsOfWorld", 0.8, int.class);
+        Constructor<Object> constructor = (Constructor<Object>) twinRobotsCT.resolveConstructor(
             numberOfColumnsOfWorldParameterMatcher, numberOfRowsOfWorldParameterMatcher);
 
         ((ClassTester<Object>) twinRobotsCT).assertConstructorValid(constructor, Modifier.PUBLIC,
             numberOfColumnsOfWorldParameterMatcher, numberOfRowsOfWorldParameterMatcher);
+        ((ClassTester<Object>) twinRobotsCT).setClassInstance(constructor.newInstance(numberOfColumnsOfWorld, numberOfRowsOfWorld));
 
-        Field robotsField = twinRobotsCT
-            .resolveAttribute(new AttributeMatcher("robots", 0.8, robotWithOffspringCT.assureClassResolved().getTheClass()));
+        Object instance = twinRobotsCT.getClassInstance();
+        Context context = contextBuilder()
+            .add("numberOfColumnsOfWorld", numberOfColumnsOfWorld)
+            .add("numberOfRowsOfWorld", numberOfRowsOfWorld)
+            .build();
 
-        assertTrue(robotsField.getType().isArray(),
-            String.format("Der Datentyp von Attribut \"%s\" ist kein Array, sollte aber ein Array sein.", robotsField.getName()));
+        assertEquals(2, Array.getLength(robotsField.get(instance)), context,
+            result -> "array robots does not have expected length");
 
-        ((ClassTester<Object>) twinRobotsCT).setClassInstance(
-            assertDoesNotThrow(() -> constructor.newInstance(numberOfColumnsOfWorld, numberOfRowsOfWorld)));
+        Object firstRobot = Array.get(robotsField.get(instance), 0);
+        Object secondRobot = Array.get(robotsField.get(instance), 1);
 
-        var twinRobotsInstance = twinRobotsCT.getClassInstance();
-        assertDoesNotThrow(() -> robotsField.setAccessible(true));
+        assertEquals(robotWithOffspringCT.getTheClass(), firstRobot.getClass(), context,
+            result -> "object at robots[0] is not of type RobotWithOffspring");
+        assertEquals(robotWithOffspring2CT.getTheClass(), secondRobot.getClass(), context,
+            result -> "object at robots[1] is not of type RobotWithOffspring2");
 
-        var field = assertDoesNotThrow(() -> robotsField.get(twinRobotsInstance));
-        assertEquals(2, Array.getLength(field), "Der Array \"robots\" hat nicht die Größe 2.");
+        H3_1_Transformers.CONSTRUCTOR_VALUES.clear();
+        H3_1_Transformers.CONSTRUCTOR_VALUES.put("robotWithOffspringParams", new ArrayList<>());
+        H3_1_Transformers.CONSTRUCTOR_VALUES.put("robotWithOffspring2Params", new ArrayList<>());
 
-        var firstRobot = Array.get(field, 0);
-        assertEquals(robotWithOffspringCT.assureClassResolved().getTheClass(), firstRobot.getClass(), "Das Objekt am "
-            + "Index 0 im Array \"robots\" ist nicht vom Typ \"RobotWithOffspring\"");
+        String className = twinRobotsCT.getTheClass().getName();
+        Class<?> clazz = testCycle.getClassLoader()
+            .loadClass(className, new ClassTransformerTemplate(className, H3_1_Transformers.CONSTRUCTOR_TRANSFORMER));
+        Constructor<?> twinRobotsConstructor = clazz.getDeclaredConstructor(int.class, int.class, Direction.class, int.class);
+        Object transformedInstance = twinRobotsConstructor.newInstance(0, 0, Direction.UP, 0);
+        System.out.println();
 
         //        var mt = new MethodTester(robotWithOffspringCT, "initOffSpring", 0.8,
         //            Modifier.PUBLIC,
@@ -94,9 +116,8 @@ public class TutorTests_H3_1 {
         //            () -> when(getAverageSpeeedMT.getTheMethod().invoke(fast_mammalia, ArgumentMatchers.anyDouble()))
         //                .thenReturn(10d),
         //            "Could not Overwrite Method.");
-
-        var secondRobot = Array.get(field, 1);
-        assertEquals(robotWithOffspring2CT.assureClassResolved().getTheClass(), secondRobot.getClass(), "Das Objekt "
+    }
+        /*assertEquals(robotWithOffspring2CT.assureClassResolved().getTheClass(), secondRobot.getClass(), "Das Objekt "
             + "am Index 1 im Array \"robots\" ist nicht vom Typ \"RobotWithOffspring2\"");
 
         // TODO: fertig schreiben
@@ -156,5 +177,5 @@ public class TutorTests_H3_1 {
         new MethodTester(twinRobotsCT.resolve(), "addToDirectionOfBothOffsprings", 1, Modifier.PUBLIC,
             void.class,
             new ArrayList<>(List.of(new ParameterMatcher("directionToBeAdded", 0, int.class)))).verify();
-    }
+    }*/
 }
